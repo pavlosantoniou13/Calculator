522469710977cd095e2e8aea9831dfc0f3c9f62e
pavlosantoniou13 <dinant11133@gmail.com>
2023-01-11 17:17:07 +0200

    final

--- main.js
+++ main.js
@@ -1,102 +1,109 @@
-const numberButtons = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('operator')
-const equal = document.querySelector("equal")
-const clear = document.querySelector("AC")
-const previousOperand = document.querySelector("previous-operand")
-const currentOperand = document.querySelector("current-operand")
-const deleteButton = document.querySelector("delete")
-
-
-function operate(num1, num2, operator){
-    if(operator === "+"){
-        return add(num1, num2)
-    }
-    else if(operator === "-"){
-        return subtract(num1, num2)
-    }
-    else if(operator === "*"){
-        return multiply(num1, num2)
-    }
-    else if(operator === "÷"){
-        return divide(num1, num2)
-    }
-    
-    
-    
-}
-
-//Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
-let display = document.querySelector("display")
-storedNumber = "";
-clickedOperator = "";
-firstNumber = "";
-let result = "";
-
-numberButtons.forEach(number => number.addEventListener('click', (e) =>{
-    storedNumber = e.target.value
-    
-
-}))
-
-
-
-operatorButton.forEach((operator => {
-    operator.addEventListener('click', function() {
-  
-      // save the first number
-      firstNumber = storedNumber;
-  
-      // get the operator that was clicked
-      clickedOperator = operator.textContent;
-      previousOperand.textContent = storedNumber + clickedOperator;
-      storedNumber = '';
-  
-      
-    })
-}));
-
-
-
-
-  
-
-
-
-
-
-
-
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
+const display = document.querySelector(".calculator-display")
+const previousOperand = document.querySelector(".previous-operand")
+const currentOperand = document.querySelector(".current-operand")
+const showResult = document.querySelector('.result');
+const equalsKey = document.querySelector('.equals-key');
+const clear = document.querySelector(".clear")
+const deleteButton = document.querySelector(".delete")
 
 
 const add = function(a, b) {
-	return a + b
+    return a + b;
 };
 
 const subtract = function(a, b) {
-	return a - b
+    return a - b;
 };
 
-function divide(a, b) {
-    return a / b;
-};
-
-
-// Multiply a, b ... 
 function multiply(a, b) {
     return a * b;
 };
 
+const divide = function(a, b) {
+    return a / b
+}
+
+function operate(num1, num2, operator){
+    if(operator === "+") {
+        return add(num1, num2)
+    }
+    else if(operator === "-") {
+        return subtract(num1, num2)
+    }
+    else if(operator === "*") {
+        return multiply(num1, num2)
+    }
+    else if(operator === "/") {
+        return divide(num1, num2)
+    }
+}
+
+let firstNumber = ""
+let clickedOperator = ""
+let storedNumber = ""
+let result = ""
+currentOperand.textContent = 0;
+
+numbers.forEach((number) => {
+    number.addEventListener('click', function() {
+        storedNumber += number.value
+        currentOperand.textContent = storedNumber
+    })
+})
+
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', function() {
+
+        if(firstNumber !== "" && storedNumber !== ""){
+            result = operate(parseFloat(firstNumber), parseFloat(storedNumber), clickedOperator)
+            
+            
+            currentOperand.textContent = ""
+        }
+
+        firstNumber = storedNumber
+        storedNumber = ""
+        clickedOperator = operator.textContent
+        previousOperand.textContent = firstNumber + " " + clickedOperator
+        
+        currentOperand.textContent = storedNumber
+        
+        if(result !== ""){
+            firstNumber = result
+            clickedOperator = operator.textContent
+            previousOperand.textContent = firstNumber + " " + clickedOperator
+            storedNumber = ''
+            currentOperand.textContent = storedNumber
+
+        }
+           
+        
+    })
+})
 
 
 
+equalsKey.addEventListener("click", function() {
+    result = operate(parseFloat(firstNumber), parseFloat(storedNumber), clickedOperator)
+    currentOperand.textContent = result
+    previousOperand.textContent = firstNumber + " " + clickedOperator + " " + storedNumber
+    
+    
+    
+})
 
+clear.addEventListener("click", function(){
+    storedNumber = ""
+    firstNumber = ""
+    currentOperand.textContent = "0"
+    previousOperand.textContent = ""
+    result = ""
+})
 
-
-
-
-
-
-
-//clear()
-
-
+deleteButton.addEventListener("click", function(){
+    storedNumber = ""
+    currentOperand.textContent = "0"
+})
